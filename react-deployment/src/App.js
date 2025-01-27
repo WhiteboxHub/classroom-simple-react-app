d13c4b2b76af27e4b847382d15e6cf0e8bbb447e
ManisaiReddy <manisaisaduvala21@gmail.com>
2025-01-27 21:08:49 +0530

    added ui

---
+++ react-deployment/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div style={{ textAlign: 'center' }}>
+      <h1>Whitebox Learning</h1>
+      <p>This institute is primarily designed for candidates seeking employment opportunities. It offers a comprehensive learning experience through both classroom and recording sessions. Additionally, it provides job assistance to ensure a smooth transition into the industry.</p>
+      <p>Whitebox Learning specializes in Artificial Intelligence (AI) and Machine Learning (ML), offering an extensive range of courses and training programs in these fields. The institute's focus on AI and ML prepares students for the rapidly growing demand in these technologies, making them highly employable in the job market.</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
